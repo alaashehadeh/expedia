e316874fb39d74019e61a754d7ac57452357d92c
Ala Shehadeh <alaa.sh.1984@gmail.com>
2017-07-05 01:21:39 +0300

    search front end page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,150 @@
 <!doctype html>
 <html lang="{{ config('app.locale') }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Expedia assignment</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- css -->
+    <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" type="text/css" rel="stylesheet"/>
+    <link href="bower_components/bootstrap-star-rating/css/star-rating.min.css" type="text/css" rel="stylesheet"/>
+    <link rel="stylesheet" href="bower_components/jquery_cityAutocomplete/city-autocomplete.min.css">
+    <link rel="stylesheet" href="bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css">
+    <link rel="stylesheet"
+          href="bower_components/eonasdan-bootstrap-datetimepicker/build/css/bootstrap-datetimepicker.min.css"/>
+    <link href="css/web.css" type="text/css" rel="stylesheet"/>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
+</head>
+<body>
+<nav class="navbar navbar-default">
+    <div class="container">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#"></a><img src="images/logo.png"/>
+        </div>
+    </div>
+</nav>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+<div class="container">
+    <form method="post">
+        <!-- city -->
+        <div class="form-group">
+            <input class="form-control" type="text" id="city" name="city" autocomplete="off" placeholder="City name">
+        </div>
 
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+        <!-- duration date -->
+        <div class="row">
+            <div class="col-md-2">Trip Date</div>
+            <div class="col-md-10 form-group">
+                <div class="row">
+                    <div class="col-md-1">From</div>
+                    <div class="col-md-5">
+                        <div class='input-group date' id='datetimepicker6'>
+                            <input type='text' class="form-control"/>
+                <span class="input-group-addon">
+                    <span class="glyphicon glyphicon-calendar"></span>
+                </span>
+                        </div>
+                    </div>
+                    <div class="col-md-1">To</div>
+                    <div class='col-md-5'>
+                        <div class="form-group">
+                            <div class='input-group date' id='datetimepicker7'>
+                                <input type='text' class="form-control"/>
+                <span class="input-group-addon">
+                    <span class="glyphicon glyphicon-calendar"></span>
+                </span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-    </body>
+
+        <!-- Hotel stars -->
+        <div class="row form-group">
+            <div class="col-md-2">Hotel Stars</div>
+            <div class="col-md-10">
+                <div class="col-md-1">From</div>
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <input id="input-1" name="input-1" class="rating rating-loading" data-min="1" data-max="5"
+                           data-step="1" data-size="xs" data-show-clear="false" data-show-caption="false">
+                </div>
+                <div class="col-md-1">To</div>
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <input id="input-2" name="input-2" class="rating rating-loading" data-min="1" data-max="5"
+                           data-step="1" data-size="xs" data-show-clear="false" data-show-caption="false">
+                </div>
+            </div>
+        </div>
+
+        <!-- Hotel Rating -->
+        <div class="row form-group">
+            <div class="col-md-2">Hotel Rating</div>
+            <div class="col-md-10">
+                <div class="col-md-1">From</div>
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <input id="input-1" name="input-1" class="rating rating-loading" data-min="1" data-max="5"
+                           data-step="1" data-size="xs" data-show-clear="false" data-show-caption="false">
+                </div>
+                <div class="col-md-1">To</div>
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <input id="input-2" name="input-2" class="rating rating-loading" data-min="1" data-max="5"
+                           data-step="1" data-size="xs" data-show-clear="false" data-show-caption="false">
+                </div>
+            </div>
+        </div>
+
+        <!-- Gust Rating -->
+        <div class="row form-group">
+            <div class="col-md-2">Guest Rating</div>
+            <div class="col-md-10">
+                <div class="col-md-1">From</div>
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <input id="input-1" name="input-1" class="rating rating-loading" data-min="1" data-max="5"
+                           data-step="1" data-size="xs" data-show-clear="false" data-show-caption="false">
+                </div>
+                <div class="col-md-1">To</div>
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <input id="input-2" name="input-2" class="rating rating-loading" data-min="1" data-max="5"
+                           data-step="1" data-size="xs" data-show-clear="false" data-show-caption="false">
+                </div>
+            </div>
+        </div>
+
+        <!-- submit -->
+        <div class="row">
+            <div class="col-md-12 text-center">
+                <button type="submit" class="btn btn-primary"><i class="glyphicon glyphicon-search"></i> Search</button>
+            </div>
+        </div>
+    </form>
+</div>
+<!-- js -->
+<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?libraries=places&language=en"></script>
+<script type="text/javascript" src="bower_components/jquery/dist/jquery.min.js"></script>
+<script type="text/javascript" src="bower_components/bootstrap-star-rating/js/star-rating.min.js"></script>
+<script src="bower_components/jquery_cityAutocomplete/jquery.city-autocomplete.js"></script>
+<script type="text/javascript" src="bower_components/moment/min/moment.min.js"></script>
+<script type="text/javascript"
+        src="bower_components/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
+<script type="text/javascript">
+    $('input#city').cityAutocomplete();
+
+    $(function () {
+        $('#datetimepicker6').datetimepicker({
+            useCurrent: false, //Important! See issue #1075
+            format: 'YYYY-MM-DD',
+            minDate: new Date()
+        });
+        $('#datetimepicker7').datetimepicker({
+            useCurrent: false, //Important! See issue #1075
+            format: 'YYYY-MM-DD',
+            minDate: new Date()
+        });
+    });
+</script>
+</body>
 </html>
